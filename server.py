826445f2494a15cde091b797dc2f49cd3220727f
Krishelle Hardson-Hurley <khardsonhurley@gmail.com>
2016-09-07 05:41:27 +0000

    Final commit before Career Day. Read Me to come.

--- server.py
+++ server.py
@@ -406,7 +406,7 @@ def add_phrase_to_db(phrase,translation):
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the point
     # that we invoke the DebugToolbarExtension
-    app.debug = True
+    app.debug = False
 
     connect_to_db(app)
 
